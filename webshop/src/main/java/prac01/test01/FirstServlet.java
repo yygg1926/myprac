6d37f9209a755cd3b20069f6d762fc6ebcb1e85a
Senbro <yygg1926@naver.com>
2022-09-16 17:41:31 +0900

    first practice project commit

---
+++ webshop/src/main/java/prac01/test01/FirstServlet.java
@@ -0,0 +1,24 @@
+package prac01.test01;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends HttpServlet {
+
+	public void init() throws ServletException {
+	      System.out.println("init 메서드 호출 First");
+	   }
+
+	   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  ServletException, IOException {
+	      System.out.println("doGet 메서드 호출 First");
+	   } 
+
+	   public void destroy() {
+	      System.out.println("destroy 메서드 호출 First");
+	   }
+	
+}
